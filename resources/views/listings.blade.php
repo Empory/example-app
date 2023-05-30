bc91de37f93ca3f591f91882ff1097f1d4a19473
Empory <oyuneverytime@gmail.com>
2023-05-30 23:47:02 +0300

    .

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -8,9 +8,7 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
 {{-- <h1>{{$heading}}</h1> --}}
 @unless(count($listings)==0)
 @foreach ($listings as $listing)
-  <h2><a href="/listings/{{$listing["id"]}}">{{$listing["title"]}}</a></h2>
-  <p>{{$listing["description"]}}</p>
-
+<x-listing-card :listing="$listing"/>
 @endforeach
 
 @else
